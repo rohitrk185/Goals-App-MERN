76be0236c1a0cad66e3113d7bb6ac2d053e4a3fc
RohitKumar *2102 <rohitrk185>
2022-10-29 07:42:38 +0530

    complete logout and login functionality. add extra reducers for login and logout and hook-up login form.

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,8 +13,26 @@ const register = async (userData) => {
     return response.data;
 }
 
+// Login user
+const login = async (userData) => {
+    console.log(API_URL);
+    const response = await axios.post(API_URL + 'login', userData);
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+    }
+
+    return response.data;
+}
+
+const logout = () => {
+    localStorage.removeItem('user');
+};
+
+
 const authService = {
     register,
+    logout,
+    login,
 }
 
 export default authService;
