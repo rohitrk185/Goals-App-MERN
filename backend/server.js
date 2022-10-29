729a7c535b5628e5e22cc5331355cc3b5c4dcd02
RohitKumar *2102 <rohitrk185>
2022-10-29 20:44:46 +0530

    change port in server.js of backend to uppercase

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 
 
